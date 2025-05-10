b2051027a6d349075675b640997f878126a09fc3
TimChen <37008451+TimChen1383@users.noreply.github.com>
2025-05-10 23:35:53 +0800

    add custom button to content browser

--- Plugins/AutomationToolkits/Source/AutomationToolkits/Private/AutomationToolkits.cpp
+++ Plugins/AutomationToolkits/Source/AutomationToolkits/Private/AutomationToolkits.cpp
@@ -1,18 +1,71 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "AutomationToolkits.h"
+#include "ContentBrowserModule.h"
 
 #define LOCTEXT_NAMESPACE "FAutomationToolkitsModule"
 
 void FAutomationToolkitsModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	InitCBMenuExtension();
 }
 
 void FAutomationToolkitsModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+}
+
+void FAutomationToolkitsModule::InitCBMenuExtension()
+{
+	//Load Content Browser module
+	FContentBrowserModule& ContentBrowserModule =
+	FModuleManager::LoadModuleChecked<FContentBrowserModule>(TEXT("ContentBrowser"));
+	
+	//Load Menu Extender
+	TArray<FContentBrowserMenuExtender_SelectedPaths>& ContentBrowserMenuExtenders =
+	ContentBrowserModule.GetAllPathViewContextMenuExtenders();
+	
+	//Create custom Delegate > Do first binding > Add Delegate to Content Browser Menu Extender list
+	//First binding: Define the position for inserting menu entry
+	ContentBrowserMenuExtenders.Add(FContentBrowserMenuExtender_SelectedPaths::CreateRaw(this, &FAutomationToolkitsModule::CustomCBMenuExtender));
+}
+
+TSharedRef<FExtender> FAutomationToolkitsModule::CustomCBMenuExtender(const TArray<FString>& SelectedPaths)
+{
+	TSharedRef<FExtender> MenuExtender(new FExtender());
+
+	if (SelectedPaths.Num() > 0)
+	{
+		//Define the position for inserting menu entry
+		MenuExtender->AddMenuExtension(
+		FName("Delete"),
+		EExtensionHook::After,
+		TSharedPtr<FUICommandList>(),
+		//Second binding: Define the dwtails for menu entry. Title, Tooltips, Function
+		FMenuExtensionDelegate::CreateRaw(this, &FAutomationToolkitsModule::AddCBMenuEntry));
+	}
+	return MenuExtender;
+}
+
+inline void FAutomationToolkitsModule::AddCBMenuEntry(class FMenuBuilder& MenuBuilder)
+{
+	//Define the dwtails for menu entry. Title, Tooltips, Function
+	MenuBuilder.AddMenuEntry
+	(
+		FText::FromString(TEXT("Delete Unused Assets")),
+		FText::FromString(TEXT("Safely delete all unused assets under folder")),
+		FSlateIcon(),
+		//Third binding: The actual function to execute
+		FExecuteAction::CreateRaw(this, &FAutomationToolkitsModule::OnDeleteUnusedAssetButtonClicked)
+		);
+}
+
+inline void FAutomationToolkitsModule::OnDeleteUnusedAssetButtonClicked()
+{
+	if(GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("This is the pre-set text"));	
+
 }
 
 #undef LOCTEXT_NAMESPACE
--- Plugins/AutomationToolkits/Source/AutomationToolkits/Public/AutomationToolkits.h
+++ Plugins/AutomationToolkits/Source/AutomationToolkits/Public/AutomationToolkits.h
@@ -11,4 +11,23 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+
+private:
+	//The level of Content Browser is lower than the normal actor class. 
+	//In order to add custom buttons to the Content Browser we need to modify through module not just actor class
+
+	//Add Custom Delegate to Content Browser Menu Extender list and do first binding
+	void InitCBMenuExtension(); 
+
+	//Define the position for inserting menu entry and do second binding
+	TSharedRef<FExtender> CustomCBMenuExtender(const TArray<FString>& SelectedPaths);
+
+	void AddCBMenuEntry(class FMenuBuilder& MenuBuilder);
+
+	void OnDeleteUnusedAssetButtonClicked();
+	
 };
+
+
+
